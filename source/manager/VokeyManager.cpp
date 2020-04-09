07486fc28e70fbda173e4e2d72e777a26aaa2ba5
nalquas <22664233+nalquas@users.noreply.github.com>
2020-04-09 18:03:14 +0200

    Opening and closing of about

--- source/manager/VokeyManager.cpp
+++ source/manager/VokeyManager.cpp
@@ -18,6 +18,11 @@
 // Libraries
 #include <iostream>
 #include <QApplication>
+#include <QAction>
+#include <QObject>
+#include <QPushButton>
+#include <QDialog>
+#include <QDialogButtonBox>
 
 // UI with layout
 #include "ui/manager.h"
@@ -25,17 +30,47 @@
 
 using namespace std;
 
+QMainWindow *manager;
+Ui_VokeyManager *ui_manager;
+QDialog *about;
+Ui_VokeyAbout *ui_about;
+
+// Function headers
+void about_open(void);
+void about_close(void);
+
+// Function implementations
+
 int main(int argc, char **argv)
 {
 	cout << "Launching VokeyManager...\n";
-
 	QApplication app(argc, argv);
 
-	QMainWindow manager;
+	// VokeyManager
+	manager = new QMainWindow;
+	ui_manager = new Ui_VokeyManager;
+	ui_manager->setupUi(manager);
 
-	Ui_MainWindow ui;
-	ui.setupUi(&manager);
+	// About Vokey
+	about = new QDialog;
+	ui_about = new Ui_VokeyAbout;
+	ui_about->setupUi(about);
 
-	manager.show();
+	// Connections: VokeyManager
+	QObject::connect(ui_manager->actionQuit, &QAction::triggered, app.closeAllWindows);
+	QObject::connect(ui_manager->actionAbout, &QAction::triggered, about_open);
+
+	// Connections: About Vokey
+	QObject::connect(ui_about->buttonBox, &QDialogButtonBox::rejected, about_close);
+
+	manager->show();
 	return app.exec();
+}
+
+void about_open() {
+	about->show();
+}
+
+void about_close() {
+	about->close();
 }

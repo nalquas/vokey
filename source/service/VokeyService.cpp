dc6a8a9ef72572b17ddde27c7f2c174c14ab493f
nalquas <22664233+nalquas@users.noreply.github.com>
2020-04-10 12:09:25 +0200

    Store pid of running service -> only one instance

--- source/service/VokeyService.cpp
+++ source/service/VokeyService.cpp
@@ -17,13 +17,19 @@
 
 #define VOKEY_CONFIG_VERSION 1
 
+#define VOKEY_TMP "/tmp/vokey"
+#define VOKEY_TMP_PID "/tmp/vokey/vokey.pid"
+#define VOKEY_TMP_LOG "/tmp/vokey/vokey.log"
+
 // Libraries
 #include <iostream>
 #include <fstream>
 #include <nlohmann/json.hpp>
 #include <stdlib.h>
+#include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 // Classes
 #include "EventRecognizer.h"
@@ -33,12 +39,12 @@ using json = nlohmann::json;
 
 EventRecognizer er;
 string config_location;
+struct stat st = {0};
 
 void ensure_config_exists() {
 	string config_path = config_location + "/config.json";
 	string profile_location = config_location + "/profiles";
 	string profile_path = profile_location + "/default_profile.json";
-	struct stat st = {0};
 
 	// Ensure vokey directory exists
 	if (stat(config_location.c_str(), &st) == -1) {
@@ -106,11 +112,66 @@ string get_default_profile_path() {
 	return config_location + "/profiles/" + string(json::parse(temp)["default_profile"]);
 }
 
+void store_pid() {
+	ofstream of;
+	of.open(VOKEY_TMP_PID);
+	of << getpid();
+	of.close();
+}
+
+void clean_tmp() {
+	// Delete PID file
+	unlink(VOKEY_TMP_PID);
+}
+
+bool already_running() {
+	// Make sure directory exists
+	if (stat(VOKEY_TMP, &st) == -1) {
+		mkdir(VOKEY_TMP, 0744);
+	}
+	
+	// Does file already exist?
+	if (stat(VOKEY_TMP_PID, &st) != -1) {
+		string temp = "";
+		pid_t pid = 0;
+		
+		ifstream ifs;
+		ifs.open(VOKEY_TMP_PID);
+		ifs >> temp;
+		ifs.close();
+
+		// Is the PID given in the file still running?
+		try
+		{
+			pid = stoi(temp);
+			if (0 == kill(pid, 0)) return true;
+		}
+		catch(const exception& e)
+		{
+			cerr << e.what() << '\n';
+			cout << "As the temporary PID file caused an error, we'll just assume there is no other instance running...\n";
+		}
+		
+	}
+
+	// Since there is no other instance running, store our PID
+	store_pid();
+	return false;
+}
+
 int main(int argc, char const *argv[]) {
+	if (already_running()) {
+		cout << "There already is an instance of vokey running. Exiting...\n";
+		return 0;
+	}
+
 	config_location = string(getenv("HOME")) + "/.config/vokey";
 	
 	ensure_config_exists();
 
 	er.load_profile(get_default_profile_path());
-	return er.run();
+	
+	int result = er.run();
+	clean_tmp(); // TODO: What about when the application is killed, do we just leave the temporary files there?
+	return result;
 }

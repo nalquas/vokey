16e9302ca1be53e5caa8398608ee10892fee2aa8
nalquas <22664233+nalquas@users.noreply.github.com>
2020-04-11 17:27:53 +0200

    Restructuring/combining of refresh functionality

--- source/manager/VokeyManager.cpp
+++ source/manager/VokeyManager.cpp
@@ -48,26 +48,27 @@ QDialog *about;
 Ui_VokeyAbout *ui_about;
 
 // Timers
-QTimer *timer_log;
+QTimer *timer_monitor;
 
-// Internal function headers
-void refresh_status(void);
+// Function headers
 
-// GUI-function headers
 void about_open(void);
 void about_close(void);
 void discard_global_settings(void);
+void refresh_log(void);
 void refresh_monitor(void);
-void reload_profile(void);
 void reset_global_settings(void);
 void save_global_settings(void);
-void set_listening(void);
+void service_reload_profile(void);
+void service_set_listening(void);
 void quit(void);
 
 // Function implementations
 
 int main(int argc, char **argv) {
 	ensure_config_exists();
+	ensure_log_exists();
+	ensure_tmp_exists();
 	load_config();
 
 	cout << "Launching VokeyManager...\n";
@@ -78,7 +79,6 @@ int main(int argc, char **argv) {
 	manager = new QMainWindow;
 	ui_manager = new Ui_VokeyManager;
 	ui_manager->setupUi(manager);
-	refresh_status();
 
 	// About Vokey
 	about = new QDialog;
@@ -88,9 +88,9 @@ int main(int argc, char **argv) {
 	// Connections: VokeyManager
 	QObject::connect(ui_manager->actionQuit, &QAction::triggered, quit);
 	QObject::connect(ui_manager->actionAbout, &QAction::triggered, about_open);
-	QObject::connect(ui_manager->pushButton_restart, &QPushButton::clicked, reload_profile);
-	QObject::connect(ui_manager->checkBox_listening, &QCheckBox::stateChanged, set_listening);
-	QObject::connect(ui_manager->pushButton_monitor_refresh, &QPushButton::clicked, refresh_status);
+	QObject::connect(ui_manager->pushButton_restart, &QPushButton::clicked, service_reload_profile);
+	QObject::connect(ui_manager->checkBox_listening, &QCheckBox::stateChanged, service_set_listening);
+	QObject::connect(ui_manager->pushButton_monitor_refresh, &QPushButton::clicked, refresh_monitor);
 	QObject::connect(ui_manager->buttonBox_global->button(QDialogButtonBox::Save), &QPushButton::clicked, save_global_settings);
 	QObject::connect(ui_manager->buttonBox_global->button(QDialogButtonBox::Discard), &QPushButton::clicked, discard_global_settings);
 	QObject::connect(ui_manager->buttonBox_global->button(QDialogButtonBox::RestoreDefaults), &QPushButton::clicked, reset_global_settings);
@@ -98,10 +98,12 @@ int main(int argc, char **argv) {
 	// Connections: About Vokey
 	QObject::connect(ui_about->buttonBox, &QDialogButtonBox::rejected, about_close);
 
-	// Start a timer for log refreshing
-	timer_log = new QTimer();
-	QObject::connect(timer_log, &QTimer::timeout, refresh_monitor);
-	ensure_log_exists();
+	// Global settings tab: Load current settings from config
+	discard_global_settings();
+
+	// Start monitor-tab refresh timer
+	timer_monitor = new QTimer();
+	QObject::connect(timer_monitor, &QTimer::timeout, refresh_monitor);
 	refresh_monitor();
 
 	// Show application to user
@@ -119,7 +121,12 @@ void about_close() {
 	about->close();
 }
 
-void refresh_monitor() {
+void discard_global_settings() {
+	ui_manager->lineEdit_default_profile->setText(QString::fromStdString(config["default_profile"]));
+	ui_manager->checkBox_global_listening->setChecked(config["listening_on_startup"]);
+}
+
+void refresh_log() {
 	// Load log from file
 	string temp = "";
 	ifstream ifs;
@@ -147,12 +154,64 @@ void refresh_monitor() {
 			sb->setValue(sb->maximum());
 		}
 	}
-
-	// Restart timer
-	timer_log->start(1500);
 }
 
-void reload_profile() {
+void refresh_monitor() {
+	// Stop the timer (just in case this was triggered by the manual refresh button)
+	timer_monitor->stop();
+
+	// Check if the service is running
+	bool service_running = (get_service_pid() >= 0);
+
+	// Only allow the listening checkbox to be interacted with if the service is running
+	ui_manager->checkBox_listening->setEnabled(service_running);
+
+	if (service_running) {
+		// If the tmp-file for listening exists, get the current listening status and show it in the listening checkbox
+		if (stat(VOKEY_TMP_LISTENING, &st) != -1) {
+			bool listening = true;
+			string temp = "";
+			ifstream ifs;
+			ifs.open(VOKEY_TMP_LISTENING);
+			ifs >> temp;
+			ifs.close();
+
+			try
+			{
+				listening = (stoi(temp) > 0);
+			}
+			catch(const exception& e)
+			{
+				cerr << e.what() << '\n';
+			}
+
+			ui_manager->checkBox_listening->setChecked(listening);
+		}
+		else {
+			// As the listening file doesn't exist yet, assume default settings
+			ui_manager->checkBox_listening->setChecked(config["listening_on_startup"]);
+		}
+
+		// Refresh the log view of the monitor tab
+		refresh_log();
+	}
+
+	// Restart the timer
+	timer_monitor->start(1500);
+}
+
+void reset_global_settings() {
+	reset_config_to_default();
+	discard_global_settings();
+}
+
+void save_global_settings() {
+	config["default_profile"] = ui_manager->lineEdit_default_profile->text().toStdString();
+	config["listening_on_startup"] = ui_manager->checkBox_global_listening->isChecked();
+	save_config();
+}
+
+void service_reload_profile() {
 	pid_t pid = get_service_pid();
 	if (pid >= 0) {
 		// Get data
@@ -172,23 +231,7 @@ void reload_profile() {
 	// TODO If the service does not exist, start it
 }
 
-void reset_global_settings() {
-	reset_config_to_default();
-	discard_global_settings();
-}
-
-void save_global_settings() {
-	config["default_profile"] = ui_manager->lineEdit_default_profile->text().toStdString();
-	config["listening_on_startup"] = ui_manager->checkBox_global_listening->isChecked();
-	save_config();
-}
-
-void discard_global_settings() {
-	ui_manager->lineEdit_default_profile->setText(QString::fromStdString(config["default_profile"]));
-	ui_manager->checkBox_global_listening->setChecked(config["listening_on_startup"]);
-}
-
-void set_listening() {
+void service_set_listening() {
 	pid_t pid = get_service_pid();
 	if (pid >= 0) {
 		// Get data
@@ -210,47 +253,4 @@ void set_listening() {
 
 void quit() {
 	application->closeAllWindows();
-}
-
-// Internal functions
-
-void refresh_status() {
-	ensure_tmp_exists();
-
-	// Monitor tab
-
-	bool service_running = (get_service_pid() >= 0);
-
-	ui_manager->checkBox_listening->setEnabled(service_running);
-
-	if (service_running) {
-		// Listening
-		bool listening = true;
-		if (stat(VOKEY_TMP_LISTENING, &st) != -1) {
-			string temp = "";
-			ifstream ifs;
-			ifs.open(VOKEY_TMP_LISTENING);
-			ifs >> temp;
-			ifs.close();
-
-			try
-			{
-				listening = (stoi(temp) > 0);
-			}
-			catch(const exception& e)
-			{
-				cerr << e.what() << '\n';
-			}
-
-			ui_manager->checkBox_listening->setChecked(listening);
-		}
-	}
-	else {
-		// Get defaults from config
-		ui_manager->checkBox_listening->setChecked(config["listening_on_startup"]);
-	}
-
-	// Global settings tab
-	ui_manager->checkBox_global_listening->setChecked(config["listening_on_startup"]);
-	ui_manager->lineEdit_default_profile->setText(QString::fromStdString(string(config["default_profile"])));
 }
